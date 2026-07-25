e91a47bd43e01e928bd549ebf64a7e219e6b38e2
Zakria-sudo <gostudyzaki@gmail.com>
2026-07-25 15:35:06 +0500

    auth middleware

---
+++ server/middlewares/auth.middleware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+import userModel from "../models/user.model";
+
+async function authorizeUser(req, res, next) {
+  let token = req.headers.authorization;
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decoded.id;
+    const user = await userModel.findById(userId);
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+    req.user = user;
+    next();
+  } catch (error) {}
+}
+authorizeUser();
